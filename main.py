6f5f0cb096b1611ef87883843ef9a4dd78f5c0c1
JNocetti <juan.anocetti@gmail.com>
2023-11-21 01:10:36 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+data = pd.read_excel('Proyecto_final_IN2023.xlsx')
+missingData = data.isnull().sum()
+
+print("Data sin imputaciones ",missingData)
+
+# Imputación de datos
+# Gender con moda
+data['Gender'].fillna(data['Gender'].mode()[0], inplace=True)
+
+# Total_Amount con media
+data['Total_Amount'].fillna(data['Total_Amount'].mean(), inplace=True)
+
+print("Data con imputaciones ",data.isnull().sum())
+
+stats = data.describe()
+print(stats)
+
+# Creamos un nuevo dataframe sin datos nulos
+data.to_excel('Proyecto_final_IN2023_update.xlsx', index=False)
+
